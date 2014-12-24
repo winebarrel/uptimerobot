729afd77949d90109d8d4e9ae6e74c2c04f92659
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-12-24 23:00:32 +0900

    Add get_account_details

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,17 @@
 require 'uptime_robot'
+
+DEFAULT_PARAMS = {
+  'apiKey' => 'ZAPZAPZAP',
+  'format' => 'json',
+  'noJsonCallback' => '1'
+}
+
+def uptime_robot
+  stubs = Faraday::Adapter::Test::Stubs.new
+
+  described_class.new(api_key: 'ZAPZAPZAP') do |faraday|
+    faraday.adapter :test, stubs do |stub|
+      yield(stub)
+    end
+  end
+end
--- spec/uptime_robot_spec.rb
+++ spec/uptime_robot_spec.rb
@@ -1,2 +1,26 @@
 describe UptimeRobot::Client do
+  describe '#get_account_details' do
+    let(:account) do
+      {"monitorLimit"=>"100",
+       "upMonitors"=>"35",
+       "downMonitors"=>"9",
+       "pausedMonitors"=>"11"}
+     end
+
+    let(:response) do
+      {"stat"=>"ok",
+       "account"=>account}
+    end
+
+    it do
+      client = uptime_robot do |stub|
+        stub.get('getAccountDetails') do |env|
+          expect(env.params).to eq DEFAULT_PARAMS
+          [200, {'Content-Type' => 'json'}, JSON.dump(response)]
+        end
+      end
+
+      expect(client.get_account_details).to eq account
+    end
+  end
 end
