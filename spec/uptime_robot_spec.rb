080203ac67e6a60d07117f205cea83163e19c602
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-12-24 23:03:53 +0900

    Add fail test case

--- spec/uptime_robot_spec.rb
+++ spec/uptime_robot_spec.rb
@@ -23,4 +23,23 @@ describe UptimeRobot::Client do
       expect(client.get_account_details).to eq account
     end
   end
+
+  context 'when stat is fail' do
+    let(:response) do
+      {"stat"=>"fail", "id"=>"101", "message"=>"apiKey is wrong"}
+    end
+
+    it do
+      client = uptime_robot do |stub|
+        stub.get('getAccountDetails') do |env|
+          expect(env.params).to eq DEFAULT_PARAMS
+          [200, {'Content-Type' => 'json'}, JSON.dump(response)]
+        end
+      end
+
+      expect {
+        client.get_account_details
+      }.to raise_error(UptimeRobot::Error, response.inspect)
+    end
+  end
 end
